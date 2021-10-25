91c5d6101e10cb37369b7c97e47f5df78fd3d6bf
TikhonovGleb <bball2011@yandex.ru>
2021-10-25 13:32:48 +0300

    Паттерн State

---
+++ StateNeutral.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace AsteroidsState
+{
+    public sealed class StateNeutral : IState
+    {
+        public StateNeutral()
+        {
+            Debug.Log("Ожидание");
+        }
+    }
+}
---
+++ StateTest.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace AsteroidsState
+{
+    public sealed class StateTest : MonoBehaviour
+    {
+        private Player _player;
+        private float _waitTime = 3.0f;
+        
+        private void Start()
+        {
+            _player = new Player(new StateNeutral());
+        }
+
+        private void Update()
+        {
+            ChangeState();
+        }
+
+        private IEnumerator ShipTimer()
+        {
+            yield return new WaitForSeconds(_waitTime);
+            _player.State = new StateManeuvering();
+
+            yield return new WaitForSeconds(_waitTime);
+            _player.State = new StateLanding();
+        }
+
+        public void ChangeState()
+        {
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                _player.State = new StateRise();
+                StartCoroutine(ShipTimer());
+            }
+        }
+    }
+}
